dd2523405c2ba89b1ef75f53d1f982906b427350
Harshith Bachimanchi <62615092+HarshithBachimanchi@users.noreply.github.com>
2023-06-21 19:14:29 +0200

    Update utils_videos.py

--- andi_datasets/utils_videos.py
+++ andi_datasets/utils_videos.py
@@ -348,9 +348,9 @@ def transform_to_video(
     # Sample
     sample = (
         optics(sequential_particle ^ sequential_particle.number_of_particles)
-        >> dt.Multiply(scale_factor)
+        >> (dt.Multiply(scale_factor) if not motion_blur_generator else dt.Multiply(1)) # Scaling is done later for motion blur
         >> sequential_background
-        >> poisson_noise
+        >> (poisson_noise if not motion_blur_generator else dt.Multiply(1)) # Noise is added later for motion blur
     )
 
     # Masks
@@ -370,14 +370,33 @@ def transform_to_video(
     )
 
     # Resolve the sample
-    _video, _masks = sequential_sample.update().resolve()
+    video, masks = sequential_sample.update().resolve()
+    video = np.array(video.to_numpy())
+    masks = np.array(masks.to_numpy())
 
-    if with_masks == True:
-        final_output = (_video, _masks)
-    elif get_vip_particles:
-        final_output = (_masks[0], *_video)
+    # Motion blur
+    if motion_blur_generator:
+
+        output_length = motion_blur_generator.output_length
+        oversamp_factor = motion_blur_generator.oversamp_factor
+        exposure_time = motion_blur_generator.exposure_time
+
+        video_reshaped = video.reshape(output_length, oversamp_factor, video.shape[1], video.shape[2], video.shape[3])
+        frames_to_select = int(exposure_time * oversamp_factor)
+        video = np.sum(video_reshaped[:, :frames_to_select], axis=1)
+
+        # Add poisson noise for each frame
+        noise = func_poisson_noise()
+        for i in range(video.shape[0]):
+            video[i] = noise(video[i] * scale_factor)
+
+
+    if with_masks == True and not motion_blur_generator:
+        final_output = (video, masks)
+    elif get_vip_particles and not motion_blur_generator:
+        final_output = (masks[0], *video)
     else:
-        final_output = _video
+        final_output = video
 
     if save_video:
         if len(final_output) == 2:
@@ -387,5 +406,5 @@ def transform_to_video(
 
         imageio.mimwrite(path, video_8bit)
 
-    return final_output
+    return np.array(final_output)
 
