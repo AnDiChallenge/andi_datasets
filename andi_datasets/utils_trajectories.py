db4126c4780c7f7beb47775ccb31ac35fb548fbe
GabrielFernandezFernandez <42147961+GabrielFernandezFernandez@users.noreply.github.com>
2025-07-28 22:27:41 +0200

    Bug fix acos(1.000001)
     The models_phenom.confinement computation of the reflection
     sometimes returns a "ValueError: math domain error".
     This is due to a floating point imprecision in the trigo method ang_vec
     when using math.acos(dot_prod/magB/magA), i.e., when it does math.acos(1.0000001).
     It can be solved by clipping as suggested here:
     https://stackoverflow.com/a/48235342
     with np.clip(cos, -1, 1) although it is now a bit slower,
     but if instead of math we use numpy arccos it is compensated.
     Drop unnecessary math import.

--- andi_datasets/utils_trajectories.py
+++ andi_datasets/utils_trajectories.py
@@ -158,8 +158,6 @@ def normalize_fGN(disp, alpha, D, T: int, deltaT : int = 1):
     return disp*np.sqrt(T)**(alpha)*np.sqrt(2*D*deltaT)
 
 # %% ../source_nbs/lib_nbs/utils_trajectories.ipynb 23
-import math
-
 class trigo():
     
     '''
@@ -241,7 +239,7 @@ class trigo():
         # Get cosine value
         cos_ = dot_prod/magA/magB
         # Get angle in radians and then convert to degrees
-        return math.acos(dot_prod/magB/magA)    
+        return np.arccos(np.clip(dot_prod/magB/magA,-1,1))
     
     def rotate_vec(vec, angle):
         return (vec[0]*np.cos(angle) + vec[1]*np.sin(angle), -vec[0]*np.sin(angle) + vec[1]*np.cos(angle))
